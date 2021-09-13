fd0288570f7d54ec82d10659e63e26be97731ba4
ryanwerner288 <80904899+ryanwerner288@users.noreply.github.com>
2021-09-13 14:56:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,8 @@
     <meta name="salesforce-client-id" content="<?php echo getenv('SALESFORCE_CLIENT_ID');?>">
     <meta name="salesforce-redirect-uri" content="https://<?php echo getenv('SALESFORCE_HEROKUAPP_URL');?>/_callback.php">
     <meta name="salesforce-mode" content="<?php echo getenv('SALESFORCE_MODE');?>">
-    <meta name="salesforce-namespace" content="<?php echo getenv('SALESFORCE_NAMESPACE');?>">
+    
+	  
     <meta name="salesforce-target" content="#sign-in-link">
     <meta name="salesforce-save-access-token" content="true">
     <meta name="salesforce-forgot-password-enabled" content="<?php echo getenv('SALESFORCE_FORGOT_PASSWORD_ENABLED');?>">
