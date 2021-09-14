d7b38407ad6338b4e4cdeb9097cc573cf72938e1
ryanwerner288 <80904899+ryanwerner288@users.noreply.github.com>
2021-09-14 10:59:42 -0500

    Update index.php

--- index.php
+++ index.php
@@ -44,98 +44,12 @@
         </div>
       </div>
     </header>
-    <section class="textured-section">
-      <h1>Curated Coffee Components</h1>
-      
-
-      <ul class="products">
-        <li>
-          <img class="product-image" src="images/products/071715_Heroku_3270-.jpg" alt="">
-          <h3 class="product-name">AeroPress Coffee Maker</h3>
-          <div class="product-button"><a id="aero_link" class="btn btn-hot" href="#" onclick="SFIDWidget.login()">Login for more info</a></div>
-        </li>
-        <li>
-          <img class="product-image" src="images/products/071715_Heroku_3263-.jpg" alt="">
-          <h3 class="product-name">MSR Reactor 2.5L Stove System</h3>          
-          <div class="product-button"><a id="reactor_link" class="btn btn-hot" href="#" onclick="SFIDWidget.login()">Login for more info</a></div>
-        </li>
-        <li>
-          <img class="product-image" src="images/products/071715_Heroku_3302.jpg" alt="">
-          <h3 class="product-name">Chemex Classic 6-cup Coffee Maker</h3>
-          <div class="product-button"><a id="chemex_link" class="btn btn-hot" href="#" onclick="SFIDWidget.login()">Login for more info</a></div>
-        </li>
-      </ul>
-
-    </section>
-    <section>
-
-      <h1>Complete Brewing Kits</h1>
-
-      <ul class="kits">
-        <li>
-          <img class="kit-image" src="images/kits/071715_Heroku_3338-Edit-.jpg" alt="">
-          <h3 class="kit-name">The Chemex Kit</h3>
-        </li>
-        <li>
-          <img class="kit-image" src="images/kits/071715_Heroku_3353-Edit-.jpg" alt="">
-          <h3 class="kit-name">The Pour Over Kit</h3>
-        </li>
-        <li>
-          <img class="kit-image" src="images/kits/071715_Heroku_3346-.jpg" alt="">
-          <h3 class="kit-name">The Camper’s Kit</h3>
-        </li>
-        <li>
-          <img class="kit-image" src="images/kits/071715_Heroku_3376-Edit-.jpg" alt="">
-          <h3 class="kit-name">The French Press Kit</h3>
-        </li>
-        <li>
-          <img class="kit-image" src="images/kits/071715_Heroku_3385-.jpg" alt="">
-          <h3 class="kit-name">The Cold Brew Kit</h3>
-        </li>
-      </ul>
-
-    </section>
-    <footer>
-
-      <div class="trailer-logo">
-        <img class="logo" src="images/fix-logotype.png" alt="Fix">
-        <br>
-        <span class="logo-text">Fine Coffee Kits</span>
-      </div>
-
-      <div class="trailer-links">
-        <ul class="internal-links">
-          <li><a href="https://heroku.github.io/fix">About</a></li>
-          <li><a href="https://heroku.github.io/fix">Support</a></li>
-          <li><a href="https://heroku.github.io/fix">Contact Us</a></li>
-        </ul>
-        <ul class="social-links">
-          <li><a href="#">
-            <img class="social-logo" src="images/social/twitter.png" alt="">
-            <span class="social-verb">Follow on</span>
-            <span class="social-name">Twitter</span></a></li>
-          <li><a href="#">
-            <img class="social-logo" src="images/social/facebook.png" alt="">
-            <span class="social-verb">Like Us on</span>
-            <span class="social-name">Facebook</span></a></li>
-          <li><a href="#">
-            <img class="social-logo" src="images/social/instagram.png" alt="">
-            <span class="social-verb">Follow on</span>
-            <span class="social-name">Instagram</span></a></li>
-        </ul>
-      </div>
-
-    </footer>
-	
+    
 
 	
 	<script>
 
-	window.onload = debuglog;
-		function debuglog() {
-			const ancestors = location.ancestorOrigins;
-		console.log(ancestors);
-		}
+	
 	function onLogin(identity) {
 		
 		var targetDiv = document.querySelector(SFIDWidget.config.target);	
